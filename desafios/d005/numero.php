700e97e1e85481b0cff17cc07650f12a747d52be
Rudson Marques <rudson@educacao.aracaju.se.gov.br>
2024-03-27 08:16:46 -0300

    Fatoração do desafio 5 com php ternário

--- desafios/d005/numero.php
+++ desafios/d005/numero.php
@@ -13,17 +13,15 @@
         <h1>Analisador de Número Real</h1>
         <?php
         $num = $_POST["n"] ?? 0;
-
-        echo "<p> Analisando o número <strong> " . number_format($num, 3, ",", ".") . " </strong>
-         informado pelo usuário: </p>";
-
         $int = (int) $num;
         $fra = $num - $int;
-
-        echo "<ul><li> A parte inteira do número é <strong>" . number_format($int, 0, ",", ".") . "</strong></li>";
-        echo "<li> A parte fracionária do número é: <strong>" . number_format($fra, 3, ",", ".") . "</strong></li></ul>";
-
+        // Ivan me orientou à abrir e fechar tags php para executar somente trechos de código php, como no exemplo abaixo, os echos foram removidos dando lugar as tags de php ternário, separando o que é html do que é php.
         ?>
+        <p> Analisando o número <strong> <?= number_format($num, 3, ",", ".") ?> </strong> informado pelo usuário: </p>
+        <ul>
+            <li> A parte inteira do número é:<strong> <?= number_format($int, 3, ",", ".") ?></strong></li>
+            <li> A parte fracionária do número é: <strong> <?= number_format($fra, 3, ",", ".") ?></strong></li>
+        </ul>
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 
